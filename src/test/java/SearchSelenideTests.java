9b42e737c5ed68851d6d8bb058bf79976b7e3c66
konstantin.iryshkov <zarechny@inbox.ru>
2021-02-28 00:04:04 +0300

    Added searchSelenideYandex test

--- src/test/java/SearchSelenideTests.java
+++ src/test/java/SearchSelenideTests.java
@@ -13,4 +13,12 @@ class PracticeFormTests {
         $(byName("q")).val("Selenide").pressEnter();
         $("#rso a").shouldHave(href("https://ru.selenide.org/"));
     }
+
+    @Test
+    void searchSelenideYandex() {
+        open("https://yandex.ru/");
+
+        $("#text").val("Selenide").pressEnter();
+        $("[data-cid='1'] a").shouldHave(href("https://ru.selenide.org/"));
+    }
 }
